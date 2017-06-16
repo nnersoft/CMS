6bc16ef6c17a1b4cabb8d211af1b443f95c41c99
nnersoft <10000@365nner.com>
2017-06-16 09:49:40 +0800

    Add files via upload

---
+++ BLL/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NnerSoft.Commom;
+using NnerSoft.Logs.DALFactory;
+using NnerSoft.Logs.IDAL;
+using NnerSoft.Logs.Model;
+namespace NnerSoft.Logs.BLL
+{
+    public class Log : ILog
+    {
+
+    }
+}
